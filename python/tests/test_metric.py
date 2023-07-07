7ff78c65237f9101dbab878059017dbf10e1d773
Andrei Shadrikov <notvuvko@gmail.com>
2023-07-08 01:26:21 +0600

    more tests

--- python/tests/test_metric.py
+++ python/tests/test_metric.py
@@ -1,4 +1,29 @@
+import numpy as np
 import pytest
+from src.solver.metric import calculate_happiness
 
 
-def test_
+def test_calculate_happiness():
+    eps = 1e-7
+    musician_pos = np.array([
+        (0, 0),
+        (1, 0),
+        (1, 1)
+    ], dtype=np.float64)
+    musician_instruments = np.array([
+        0,
+        0,
+        1
+    ], dtype=np.int32)
+    attendee_positions = np.array([
+        (2, 0),
+        (2, 2)
+    ], dtype=np.float64)
+    attendee_tastes = np.array([
+        (1, -1),
+        (-1, 1)
+    ], dtype=np.float64)
+    assert np.abs(calculate_happiness(musician_pos[:1], musician_instruments[:1],
+                                      attendee_positions[:1], attendee_tastes[:1, :1]) - 0.25) < eps
+    assert np.abs(calculate_happiness(musician_pos[:2], musician_instruments[:2],
+                                      attendee_positions[:1], attendee_tastes[:1, :1]) - 1) < eps
