5ac83544fe91afc7f29fe68c02c445f4cef506df
Andrei Shadrikov <notvuvko@gmail.com>
2023-07-07 20:07:08 +0600

    placements

---
+++ python/src/solver/placement.py
@@ -0,0 +1,25 @@
+import numpy as np
+import typing as t
+
+
+def get_placements(width: float, height: float, min_distance: float = 10.):
+    return np.meshgrid(np.arange(min_distance, width - min_distance, min_distance),
+                       np.arange(min_distance, height - min_distance, min_distance))
+
+
+def get_placements_hexagonal(width: float, height: float, min_distance: float = 10.):
+    xs, ys = np.meshgrid(np.arange(min_distance, width - min_distance, min_distance),
+                         np.arange(min_distance, height - min_distance, np.sqrt(3) * min_distance / 2))
+    xs[::2, :] += min_distance / 2
+    return xs, ys
+
+
+def filter_placements(
+        placements: t.Iterable[t.Tuple[float, float]],
+        width: float,
+        height: float,
+        min_distance: float = 10.
+) -> t.List[t.Tuple[float, float]]:
+    return list(filter(lambda pl: min_distance < pl[0] < (width - min_distance)
+                                  and min_distance < pl[1] < (height - min_distance),
+                       placements))
