53ee90ed9d74c3ba7be48dfef4baa35facaa6e87
Andrei Shadrikov <notvuvko@gmail.com>
2023-07-08 00:10:39 +0600

    it's not about lines, it's all about segments

--- python/src/solver/geometry.py
+++ python/src/solver/geometry.py
@@ -1,9 +1,61 @@
 import numpy as np
+from numba import njit
+import typing as t
 
 
+@njit
 def get_line_coefficients(left_points: np.ndarray, right_points: np.ndarray) -> np.ndarray:
     c = (left_points[:, 0] - right_points[:, 0]) * left_points[:, 1]\
         + (right_points[:, 1] - left_points[:, 1]) * left_points[:, 0]
     return np.concatenate((left_points[:, 1] - right_points[: 1],
                            right_points[:, 0] - left_points[:, 0],
                            c), axis=1)
+
+
+@njit
+def distance_to_line(line_coefficients: np.ndarray, points: np.ndarray) -> np.ndarray:
+    normalizer = np.sqrt(line_coefficients[:, 0] ** 2 + line_coefficients[:, 1] ** 2)
+    points = np.concatenate((points, np.ones((points.shape[0], 1))), axis=1)
+    return np.abs(line_coefficients @ points.T) / normalizer
+
+
+# https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment/6853926#6853926
+@njit
+def distance_to_segment(x: float, y: float, x1: float, y1: float, x2: float, y2: float):
+    A = x - x1
+    B = y - y1
+    C = x2 - x1
+    D = y2 - y1
+
+    dot = A * C + B * D
+    len_sq = C * C + D * D
+    param = -1
+    if len_sq != 0:  # in case of 0 length line
+        param = dot/len_sq
+
+    if param < 0:
+        xx = x1
+        yy = y1
+    elif param > 1:
+        xx = x2
+        yy = y2
+    else:
+        xx = x1 + param * C
+        yy = y1 + param * D
+
+    dx = x - xx
+    dy = y - yy
+    return np.sqrt(dx * dx + dy * dy)
+
+
+@njit
+def distances_to_segments(points: np.ndarray, segments: t.Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
+    distances = np.zeros((points.shape[0], segments[0].shape[0]), dtype=np.float64)
+    for cur_point_idx in range(points.shape[0]):
+        for cur_segment_idx in range(segments[0].shape[0]):
+            distances = distance_to_segment(
+                points[cur_point_idx, 0], points[cur_point_idx, 1],
+                segments[0][cur_segment_idx, 0], segments[0][cur_segment_idx, 1],
+                segments[1][cur_segment_idx, 0], segments[1][cur_segment_idx, 1]
+            )
+    return distances
--- python/src/solver/metric.py
+++ python/src/solver/metric.py
@@ -1,6 +1,7 @@
 import numpy as np
-import scipy as sp
-from scipy.spatial.distance import cdist
+from numba import njit
+from scipy.spatial.distance import cdist, pdist
+from src.solver.geometry import get_line_coefficients, distance_to_line
 import typing as t
 
 
@@ -10,3 +11,44 @@ def distance_singular_sqr(place_a: np.ndarray, place_b: np.ndarray) -> float:
 
 def distance(places_a: np.ndarray, places_b: np.ndarray) -> np.ndarray:
     return cdist(places_a, places_b, 'euclidean')
+
+
+def check_positions_valid(
+        musician_positions: np.ndarray,
+        xmin: float,
+        xmax: float,
+        ymin: float,
+        ymax: float,
+        min_distance: float = 10.
+) -> bool:
+    distances = pdist(musician_positions, metric='euclidean')
+    if np.any(distances < min_distance):
+        return False
+    xmin += min_distance
+    ymin += min_distance
+    xmax -= min_distance
+    ymax -= min_distance
+    return np.all(
+        (musician_positions[:, 0] >= xmin) & (musician_positions[:, 0] <= xmax)
+        & (musician_positions[:, 1] >= ymin) & (musician_positions[:, 1] <= ymax)
+    )
+
+
+@njit
+def calculate_happiness(musicians: np.ndarray, attendees: np.ndarray, attendee_tastes: np.ndarray) -> float:
+    """
+    :param musicians: NDArray with Mx3 shape. Every row is (X, Y, Instrument)
+    :param attendees: NDArray with Ax2 shape. Every row is (X, Y)
+    :param attendee_tastes: NDArray with AxT shape. Every row is tastes for a particular attendee
+    :return: happiness score
+    """
+    total_happiness = 0
+    for cur_attendee_idx in range(attendees.shape[0]):
+        line_coefficients = get_line_coefficients(
+            musicians[:, :2],
+            np.tile(attendees[cur_attendee_idx:cur_attendee_idx+1, :2], (musicians.shape[0], 1))
+        )
+        musician_blocked = np.all(distance_to_line(line_coefficients, musicians[:, :2]) > 5, axis=1)
+        cur_tastes = attendee_tastes[cur_attendee_idx]
+        attendee_happiness = cur_tastes[musicians[:, 2]] * musician_blocked
+
